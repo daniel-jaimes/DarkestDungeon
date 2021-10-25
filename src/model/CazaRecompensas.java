435bf6391c0a364abc9d1beabb207139a0c16ee5
Daniel J <dajaca00@gmail.com>
2021-10-25 21:53:23 +0200

    finish habilities and they decription

--- src/model/CazaRecompensas.java
+++ src/model/CazaRecompensas.java
@@ -20,31 +20,36 @@ public class CazaRecompensas extends Character {
     @Override
     public int habilidad1(){
         recoverStress(1);
+        System.out.println(descripcionHabilidad1());
         return 1;
     }
     @Override
     public int habilidad2(){
-        int dano, dado1, dado2;
+        int hurt, dado1, dado2;
         Random rnd = new Random();
         dado1 = rnd.nextInt(6) + 1;
         dado2 = rnd.nextInt(6) + 1;
-        dano = dado1 + dado2;
-        if(dano == 7) return -5;
+        hurt = dado1 + dado2;
+        if(hurt == 7) {
+            System.out.println(descripcionHabilidad2());
+            return -5;
+        }
         return super.habilidad2();
     }
     @Override
     public int habilidad3(){
+        System.out.println(descripcionHabilidad3());
         return -1;
     }
     //DESCRIPCIONES DE HABILIDAD - TO DO
     private String descripcionHabilidad1() {
-        return "Habilidad1 - no tiene efecto";
+        return "Recupera 1 de estres y 1 de dano";
     }
     private String descripcionHabilidad2() {
-        return "Habilidad2 - no tiene efecto";
+        return "Menos 5 de dano";
     }
     private String descripcionHabilidad3() {
-        return "Habilidad3 - no tiene efecto";
+        return "Menos 1 de dano";
     }
 
 }
